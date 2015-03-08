3db897d09f0e08fd10e248a43ab104fe4980a55d
2phar <t_shizle@yahoo.com>
2015-03-07 19:22:18 -0600

    Added Line Generator to label the lines

--- schedule_builder.py
+++ schedule_builder.py
@@ -1,9 +1,25 @@
 from bs4 import BeautifulSoup
 
+
+def line_number_gen():
+    number = 0
+    while True:
+        number += 1
+        yield number
+
+line_number = line_number_gen()
 f = file("sarahs_schedule.html")
 soup = BeautifulSoup(f)
 soup.prettify()
-print(soup.get_text())
-
-for row in soup.find_all("table"):
-	print(row.prettify())
+soup.encode('ascii', 'ignore')
+soup.find_all('table')
+for row in soup.find_all("td"):
+    # print(str(row['class']))
+    if str(row['class']) == "[u'line-content']":
+        if len(row.contents) == 1 and row.get_text().strip() != "":
+            row.replace("&nbsp", "")
+            print next(line_number), row.get_text().strip()
+        elif len(row.contents) > 1:
+            for child in row.contents:
+                if child != "":
+                    print "Line", next(line_number), child
