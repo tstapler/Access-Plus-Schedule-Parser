e7db4f772766a36055d19c8ec5dcc3a5082cb720
Tyler Stapler <tstapler@iastate.edu>
2015-03-09 20:47:25 -0500

    Refactored, the program is now printing the desired output. Now to find the best way to parse

--- schedule_builder.py
+++ schedule_builder.py
@@ -1,50 +1,28 @@
 from bs4 import BeautifulSoup
 
-
-def rm_chars(string, *args):
-    string = string.strip()
-    for char in args:
-        print(char)
-        string.replace(char, "")
-    return string
-
-
 def stripAllTags(html):
         if html is None:
                 return None
         return ''.join( BeautifulSoup(html).findAll(text= True ) )
 
-def line_number_gen():
-    number = 0
-    while True:
-        number += 1
-        yield number
 
-line_number = line_number_gen()
 f = open("sarahs_schedule.html")
 soup = BeautifulSoup(f)
 soup.prettify()
-soup.encode('ascii', 'ignore')
-flag = False 
-for row in soup.find_all("td"):
-
-    if str(row['class']) == "[u'line-content']":
-        row_text = row.get_text().strip().replace("&nbsp;", "")
-        if row_text == \
-        "For tips on printing the class schedule, click help in upper right corner.":
-            flag = True
-        elif len(row.contents) == 1 and row_text != "" and flag:
-            print(row_text.encode('utf-8'))
+soup.encode('utf-8', 'ignore')
 flag = False
+
 for span in soup.find_all("td"):
     try:
-        span_text = stripAllTags(span.get_text(strip=True).encode('utf-8'))
-        if span_text == "Instructor":
+        span_text = stripAllTags(span.get_text(strip=True).encode('ascii', 'replace'))
+        if "section" in span_text:
             flag = True
-        elif span_text == "Credits" and flag:
+            print(span_text)
+        elif "FOOTER" in span_text and flag:
             flag = False
+        elif "%=" in span_text and flag:
+            continue
         elif span_text != "" and flag:
             print(span_text)
-        
     except:
-        pass
+        continue
