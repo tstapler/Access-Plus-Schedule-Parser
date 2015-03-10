79a5d14869aad384c984edfef716199ca4b8deac
2phar <t_shizle@yahoo.com>
2015-03-09 22:39:04 -0500

    Current Progress on parsing the output text

--- schedule_builder.py
+++ schedule_builder.py
@@ -1,28 +1,83 @@
 from bs4 import BeautifulSoup
-
+import re
 def stripAllTags(html):
         if html is None:
                 return None
         return ''.join( BeautifulSoup(html).findAll(text= True ) )
 
+def section(string, numb):
+    #Return the Section identifier
+    results = re.match(field_patterns[0],string)
+    fields["section"].append(results.group(1))
+    numb += 1
+    return numb
 
+def courseName(string, numb):
+    fields["course_name"].append(string)
+    numb += 1
+    return numb
+
+def credits(string, numb):
+    results = re.match(field_patterns[1],string)
+    if(results):
+        fields["credits"].append(results.group(1))
+
+    elif string == "Credits":
+        numb += 1
+
+    return numb
+
+def requirements_meeting_dates(string, numb):
+    results = re.match(field_patterns[2],string)
+    if results:
+        fields["meeting_dates"].append((results.group(0),results.group(1)))
 f = open("sarahs_schedule.html")
 soup = BeautifulSoup(f)
 soup.prettify()
 soup.encode('utf-8', 'ignore')
-flag = False
 
+printing = False
+
+class_numb = 0
+field_numb = 0
+
+fields = {
+   "section": [],
+    "course_name": [],
+    "credits": [],
+    "recs": [],
+    "meeting_dates": [],
+    "days": [],
+    "start_time": [],
+    "end_time": [],
+    "location": []
+}
+
+field_patterns = [
+    re.compile('section *(.*)'),
+    re.compile('(\d\.\d)'),
+    re.compile('^(\d\d/\d\d/\d\d)-(\d\d/\d\d/\d\d)')
+]
+
+options = {
+    0 : section,
+
+}
 for span in soup.find_all("td"):
+
     try:
         span_text = stripAllTags(span.get_text(strip=True).encode('ascii', 'replace'))
         if "section" in span_text:
-            flag = True
-            print(span_text)
-        elif "FOOTER" in span_text and flag:
-            flag = False
-        elif "%=" in span_text and flag:
+            printing = True
+            print(span_text.strip())
+            class_numb += 1
+        elif "FOOTER" in span_text and printing:
+            printing = False
+        elif "%=" in span_text and printing:
             continue
-        elif span_text != "" and flag:
-            print(span_text)
+        elif span_text != "" and printing:
+
+            print(span_text.strip())
     except:
         continue
+
